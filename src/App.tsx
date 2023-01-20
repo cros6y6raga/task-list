fb52931ec3495f25d74060970d28025aa8b7f3e7
cros6y6raga <crosbybraga@gmail.com>
2023-01-20 19:18:09 +0300

    Added a component and useState to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Tasklist} from "./components/Tasklist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [task, setTask] = useState([
+        {id: 1, name: 'Go to the dentist', isDone: false},
+        {id: 2, name: 'Go to the gym', isDone: false},
+        {id: 3, name: 'Learning a programming language', isDone: true},
+    ])
+    return (
+        <div className="App">
+            <Tasklist/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Tasklist.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Tasklist = () => {
+    return (
+        <div>
+            <input type="text"/>
+            <button>+</button>
+            <div>
+                <input type="checkbox"/>
+            </div>
+        </div>
+    );
+};

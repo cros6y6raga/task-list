78e47276e7d69a726f9277a7fe4587a8b0262d57
cros6y6raga <crosbybraga@gmail.com>
2023-01-21 18:07:26 +0300

    I installed the uuid library and replaced the id with v1()

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Tasklist} from "./components/Tasklist";
+import {v1} from "uuid";
 
 function App() {
     const [task, setTask] = useState([
-        {id: 1, name: 'Go to the dentist', isDone: false},
-        {id: 2, name: 'Go to the gym', isDone: false},
-        {id: 3, name: 'Learning a programming language', isDone: true},
+        {id: v1(), name: 'Go to the dentist', isDone: false},
+        {id: v1(), name: 'Go to the gym', isDone: false},
+        {id: v1(), name: 'Learning a programming language', isDone: true},
     ])
     return (
         <div className="App">

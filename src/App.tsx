8bfa6e11e48e4ab8c0a719871d2c02011d7498fb
cros6y6raga <crosbybraga@gmail.com>
2023-01-21 18:50:07 +0300

    I added a filter function and made filter typing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {Tasklist} from "./components/Tasklist";
 import {v1} from "uuid";
 
+type FilterValueType= 'all' | 'active' | 'completed'
+
 function App() {
     const [task, setTask] = useState([
         {id: v1(), name: 'Go to the dentist', isDone: false},
@@ -12,6 +14,9 @@ function App() {
     const removeTask = (id: string) => {
         const remove = task.filter(el => el.id !== id)
         setTask(remove)
+    }
+    const filterTask = (value:FilterValueType) => {
+
     }
     return (
         <div className="App">

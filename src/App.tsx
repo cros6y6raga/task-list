e2ef78a480fe3c66129862ebfdd71710bd1498af
cros6y6raga <crosbybraga@gmail.com>
2023-01-23 16:57:04 +0300

    Wrote comments in the App file

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,30 @@ import React, {useState} from 'react';
 import './App.css';
 import {ITaskArray, Tasklist} from "./components/Tasklist";
 import {v1} from "uuid";
-
+// Типизация фильтрации тасок
 export type FilterValueType = 'all' | 'active' | 'completed'
 
 function App() {
-
+    // Локальный стейт тасок
     const [tasks, setTasks] = useState<ITaskArray[]>([
         {id: v1(), name: 'Go to the dentist', isDone: false},
         {id: v1(), name: 'Go to the gym', isDone: false},
         {id: v1(), name: 'Learning a programming language', isDone: true},
     ])
-
+    // Локальный стейт фильтрации тасок
     const [filter, setFilter] = useState<FilterValueType>('all')
-
+    // Функция удаления таски
     const removeTask = (id: string) => {
         const remove = tasks.filter(el => el.id !== id)
         setTasks(remove)
     }
-
+    // Функция добавления таски
     const addTask = (title: string) => {
         const task = {id: v1(), name: title, isDone: false}
         const newTask = [task, ...tasks]
         setTasks(newTask)
     }
-
+    // Фильтрация тасок
     let filteredTasks = tasks
     if (filter === 'active') {
         filteredTasks = tasks.filter(el => !el.isDone)
@@ -36,8 +36,9 @@ function App() {
     const filterTasks = (value: FilterValueType) => {
         setFilter(value)
     }
-
+    // Возврат JSX элементов
     return (
+        // Компанента и передача в компаненту данных
         <div className="App">
             <Tasklist
                 title={'Tasklist'}

6bdb2fb42a01ba29b6eab9a9690feba8465e2557
cros6y6raga <crosbybraga@gmail.com>
2023-02-07 10:26:42 +0300

    Typing for TodolistsType

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import {v1} from "uuid";
 
 // Типизация фильтрации тасок
 export type FilterValueType = 'all' | 'active' | 'completed'
+export type TodolistsType = {
+    id: string
+    title: string
+    filter: FilterValueType
+}
 
 function App() {
 
@@ -15,8 +20,8 @@ function App() {
     //     {id: v1(), name: 'Learning a programming language', isDone: true},
     // ])
 
-    let todolistID1=v1();
-    let todolistID2=v1();
+    let todolistID1 = v1();
+    let todolistID2 = v1();
 
     let [todolists, setTodolists] = useState<Array<TodolistsType>>([
         {id: todolistID1, title: 'What to learn', filter: 'all'},
@@ -24,14 +29,14 @@ function App() {
     ])
 
     let [tasks, setTasks] = useState({
-        [todolistID1]:[
+        [todolistID1]: [
             {id: v1(), title: "HTML&CSS", isDone: true},
             {id: v1(), title: "JS", isDone: true},
             {id: v1(), title: "ReactJS", isDone: false},
             {id: v1(), title: "Rest API", isDone: false},
             {id: v1(), title: "GraphQL", isDone: false},
         ],
-        [todolistID2]:[
+        [todolistID2]: [
             {id: v1(), title: "HTML&CSS2", isDone: true},
             {id: v1(), title: "JS2", isDone: true},
             {id: v1(), title: "ReactJS2", isDone: false},

29c694497be9cb41fbe5eff8e783680b5f558128
cros6y6raga <crosbybraga@gmail.com>
2023-02-07 10:14:59 +0300

    I added an associative array

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,36 @@ export type FilterValueType = 'all' | 'active' | 'completed'
 function App() {
 
     // Локальный стейт тасок
-    const [tasks, setTasks] = useState<ITaskArray[]>([
-        {id: v1(), name: 'Go to the dentist', isDone: false},
-        {id: v1(), name: 'Go to the gym', isDone: false},
-        {id: v1(), name: 'Learning a programming language', isDone: true},
+    // const [tasks, setTasks] = useState<ITaskArray[]>([
+    //     {id: v1(), name: 'Go to the dentist', isDone: false},
+    //     {id: v1(), name: 'Go to the gym', isDone: false},
+    //     {id: v1(), name: 'Learning a programming language', isDone: true},
+    // ])
+
+    let todolistID1=v1();
+    let todolistID2=v1();
+
+    let [todolists, setTodolists] = useState<Array<TodolistsType>>([
+        {id: todolistID1, title: 'What to learn', filter: 'all'},
+        {id: todolistID2, title: 'What to buy', filter: 'all'},
     ])
+
+    let [tasks, setTasks] = useState({
+        [todolistID1]:[
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistID2]:[
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+            {id: v1(), title: "ReactJS2", isDone: false},
+            {id: v1(), title: "Rest API2", isDone: false},
+            {id: v1(), title: "GraphQL2", isDone: false},
+        ]
+    });
 
     // Локальный стейт фильтрации тасок
     const [filter, setFilter] = useState<FilterValueType>('all')

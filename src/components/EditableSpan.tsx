2ef4d21941becf048d8573978ec56a6b28b9ad84
cros6y6raga <crosbybraga@gmail.com>
2023-03-06 12:47:19 +0300

    onBlur, autoFocus

--- src/components/EditableSpan.tsx
+++ src/components/EditableSpan.tsx
@@ -5,8 +5,11 @@ type PropsType = {
 }
 export const EditableSpan = (props: PropsType) => {
     const [edit, setEdit] = useState(false)
+    const editFooHandler = () => {
+        setEdit(!edit)
+    }
     return (
-        edit ? <input value={props.title}/>
-            : <span>ss</span>
+        edit ? <input value={props.title} onBlur={editFooHandler} autoFocus/>
+            : <span onDoubleClick={editFooHandler}>ss</span>
     );
 };

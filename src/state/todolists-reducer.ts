1cf1b33a61e0beb2f4ba17b95db6d636441c8345
cros6y6raga <crosbybraga@gmail.com>
2023-03-14 15:49:23 +0300

    Filled in the contents of the reducer

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,15 @@
+import {TodolistsType} from "../App";
+
+type ActionType = {
+    type: string
+    [key: string]: any
+}
+
+export const todolistsReducer = (state: Array<TodolistsType>, action: ActionType) => {
+    switch (action.type) {
+        case 'XXX':
+            return state
+        default:
+            throw new Error('I don\'t understand this type')
+    }
+};

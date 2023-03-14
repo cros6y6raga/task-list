a6ce07cd72447c0f4c5f3ca80fc6101f9d47a2e1
cros6y6raga <crosbybraga@gmail.com>
2023-03-14 16:35:00 +0300

    Wrote a test and reducer to remove todolist

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,15 +1,30 @@
 import {TodolistsType} from "../App";
 
-type ActionType = {
-    type: string
-    [key: string]: any
-}
+// type ActionType = {
+//     type: string
+//     [key: string]: any
+// }
 
-export const todolistsReducer = (state: Array<TodolistsType>, action: ActionType) => {
+export const todolistsReducer = (state: Array<TodolistsType>, action: MainType) => {
     switch (action.type) {
-        case 'XXX':
-            return state
+        case "REMOVE-TODOLIST":{
+            return state.filter(el => el.id !== action.payload.id)
+        }
         default:
-            throw new Error('I don\'t understand this type')
+            //throw new Error('I don\'t understand this type')
+            return state
     }
 };
+
+type MainType=removeTodolistACType
+
+type removeTodolistACType = ReturnType<typeof removeTodolistAC>
+
+export const removeTodolistAC = (id: string) => {
+    return {
+        type: 'REMOVE-TODOLIST',
+        payload: {
+            id
+        }
+    } as const
+}

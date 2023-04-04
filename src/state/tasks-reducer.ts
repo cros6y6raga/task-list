611daa990caef5418e1455b76c20c4fdc74a5daa
cros6y6raga <crosbybraga@gmail.com>
2023-04-04 12:33:58 +0300

    Satisfied with the task removal test

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,11 +1,14 @@
 import {FilterValueType, TasksStateType, TodolistsType} from "../App";
 import {v1} from "uuid";
 
-export const todolistsReducer = (state: TasksStateType, action: MainType) => {
+export const tasksReducer = (state: TasksStateType, action: MainType) => {
     switch (action.type) {
-        case "":
-            return state
-        case "":
+        case 'REMOVE-TASK':
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId].filter(t => t.id !== action.taskId)
+            }
+        case '':
             return state
         default:
             throw new Error('I dont understand this type')
@@ -17,7 +20,7 @@ type MainType = RemoveTaskActionType | SecondActionType
 type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
 type SecondActionType = ReturnType<typeof secondAC>
 
-export const removeTaskAC = (taskId: string,todolistId:string) => {
+export const removeTaskAC = (taskId: string, todolistId: string) => {
     return {
         type: 'REMOVE-TASK',
         taskId,

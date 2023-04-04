574c75d23996f9b62823dc12c2c9565d6ad77c3c
cros6y6raga <crosbybraga@gmail.com>
2023-04-04 12:53:18 +0300

    Ref: task status test

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -75,6 +75,6 @@ test('status of specified task should be changed', () => {
 
     const endState = tasksReducer(startState, action)
 
-    expect().toBe()
-    expect().toBe()
+    expect(endState['todolistId2'][1].isDone).toBe(false)
+    expect(endState['todolistId1'][1].isDone).toBe(true)
 })
